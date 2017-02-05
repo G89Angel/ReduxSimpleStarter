bc923d95205976bb8d515731553f009663a0d810
Ángel M. Marqués Ruiz <g89.angel@gmail.com>
2017-02-05 20:24:08 +0100

    Add map to each search

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
 import _ from 'lodash';
+import GoogleMap from '../components/google-map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -10,10 +11,11 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressure = cityData.list.map(weather => weather.main.pressure);
     const humidity = cityData.list.map(weather => weather.main.humidity);
+    const {lon, lat} = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="ºC"/></td>
         <td><Chart data={pressure} color="green" units="hPa"/></td>
         <td><Chart data={humidity} color="black" units="%"/></td>
